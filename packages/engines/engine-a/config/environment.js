802c9c25d959a8b1e0c724459448ae1944d0da51
mposvar <mposvar@hotmail.com>
2020-05-01 18:31:59 -0500

    initial

---
+++ packages/engines/engine-a/config/environment.js
@@ -0,0 +1,13 @@
+// config/environment.js
+
+/*jshint node:true*/
+'use strict';
+
+module.exports = function(environment) {
+  const ENV = {
+    modulePrefix: 'engine-a',
+    environment: environment
+  }
+
+  return ENV;
+};
---
+++ packages/engines/engine-a/index.js
@@ -0,0 +1,11 @@
+// index.js
+
+/*jshint node:true*/
+const EngineAddon = require('ember-engines/lib/engine-addon');
+
+module.exports = EngineAddon.extend({
+  name: require('./package').name,
+  lazyLoading: Object.freeze({
+    enabled: true
+  })
+});
